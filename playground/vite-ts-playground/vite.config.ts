94540019088a0b25ac05052493e9ad3be5d873d4
adelpro <adelpro@gmail.com>
2026-01-28 15:23:53 +0100

    build(vite): add module aliases for react, web, and core packages

--- playground/vite-ts-playground/vite.config.ts
+++ playground/vite-ts-playground/vite.config.ts
@@ -7,6 +7,18 @@ export default defineConfig({
   resolve: {
     alias: {
       "open-quran-view": path.resolve(__dirname, "../../dist"),
+      "open-quran-view/react": path.resolve(
+        __dirname,
+        "../../dist/view/react/index.js"
+      ),
+      "open-quran-view/web": path.resolve(
+        __dirname,
+        "../../dist/view/web/index.js"
+      ),
+      "open-quran-view/core": path.resolve(
+        __dirname,
+        "../../dist/core/index.js"
+      ),
     },
   },
   server: {
